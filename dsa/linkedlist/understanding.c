077f73be2d1e6a1923c51e14bb86ea9bf89d5e98
ojas R <ojasrahate20@gmail.com>
2025-08-12 00:21:25 +0530

    first commit

---
+++ dsa/linkedlist/understanding.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int value;
+    struct node* next ;
+
+};
+
+struct linkedlist
+{
+    struct node* head;
+};
+//successfully exe 
+
+void insertAtfront()
+{
+    struct linked list 
+}
---
+++ webdevp/script.js
@@ -0,0 +1,1 @@
+    alert("wlco to sigma ")
